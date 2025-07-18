d6da3d3011a5d523e1226e43fbf690e703523c1b
MashrafiGit <mdmasrafi902@gmail.com>
2025-07-18 17:28:03 +0600

    linked list reversing

---
+++ linked_list_reversing.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void linked_list_insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void linked_list_print(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << endl;
+        tmp = tmp->next;
+    }
+}
+
+void linked_list_reverse(Node *&head, Node *&tail, Node *tmp)
+{
+    if (tmp->next == NULL)
+    {
+        head = tmp;
+        return;
+    }
+    linked_list_reverse(head, tail, tmp->next);
+    tmp->next->next = tmp;
+    tmp->next = NULL;
+    tail = tmp;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        linked_list_insert_at_tail(head, tail, val);
+    }
+
+    linked_list_reverse(head, tail, head);
+    linked_list_print(head);
+
+    return 0;
+}
