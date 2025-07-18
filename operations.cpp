aa37195625b666678c109f2f644560af0bc5d795
MashrafiGit <mdmasrafi902@gmail.com>
2025-07-18 12:08:08 +0600

    operations

---
+++ operations.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    list<int>l={60, 10, 20, 40, 60, 100, 100, 20};
+
+    // l.remove(20);
+
+    // l.sort();
+    // l.sort(greater<int>());
+
+    // l.unique();
+
+    // l.reverse();
+
+    for (int val : l)
+    {
+        cout << val << " ";
+    }
+
+    return 0;
+}
